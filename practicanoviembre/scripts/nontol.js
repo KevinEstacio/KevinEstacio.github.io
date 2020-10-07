832536239109728686d20b464491009ab842407f
Kevin Estacio <knontole15_2@unc.edu.pe>
2020-10-07 09:33:20 -0500

    commit fin de clase

---
+++ practicanoviembre/scripts/nontol.js
@@ -0,0 +1,18 @@
+let flag = 1;
+
+
+function pageLoad() {
+    let bo = document.getElementById("b1");
+
+    bo.onclick = lec;
+
+}
+
+
+function extraerDatos() {
+
+}
+
+
+
+window.onload = pageLoad;
